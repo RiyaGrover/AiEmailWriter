4abf334256456ab838763da0f2f2e74e4d8eb592
Riya Grover <riyagrover@Riyas-MacBook-Air.local>
2025-07-08 01:39:22 +0530

    backend api integration

---
+++ src/main/java/com/email/aiEmail/controllers/EmailGeneratorController.java
@@ -0,0 +1,27 @@
+package com.email.aiEmail.controllers;
+
+import com.email.aiEmail.Service.EmailGeneratorService;
+import com.email.aiEmail.entity.EmailRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+public class EmailGeneratorController {
+
+    @Autowired
+    private EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
